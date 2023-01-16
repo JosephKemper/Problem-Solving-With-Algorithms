a3211a85e2b527db7d7fff458b11a967f8b3e499
JosephKemper <JosephKemper@gmail.com>
2023-01-16 07:05:09 -0700

    added code to print fraction

--- Chapter1.py
+++ Chapter1.py
@@ -3,3 +3,10 @@ class Fraction:
     def __init__(self,top, bottom) -> None:
         self.numerator = top # int
         self.denominator = bottom # int
+
+    def show (self):
+        print(self.numerator,"/",self.denominator)
+
+myFraction = Fraction(3,5)
+myFraction.show()
+
