e8981044d415292e7a6856b342052997948f87da
JosephKemper <JosephKemper@gmail.com>
2023-01-19 07:20:16 -0700

    Wrote AndGate class

--- Chapter1.py
+++ Chapter1.py
@@ -74,7 +74,18 @@ class UnaryGate (LogicGate):
         return int (input("Enter Pin input for gate " +\
             self.getLabel()+ "-->"))
 
+class AndGate (BinaryGate):
 
+    def __init__(self, n):
+        super().__init__(n)
+
+    def performGateLogic (self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a==1 and b==1:
+            return 1
+        else:
+            return 0
 
 myFraction = Fraction(3,5)
 
